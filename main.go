7ec7bad5bb69bcd05aadba57643262b9b9331a3a
xl92@DESKTOP-F8KSG17 <xl92@mail.ru>
2023-04-13 17:49:55 +0300

    добавление нескольких маршрутов

--- main.go
+++ main.go
@@ -6,12 +6,26 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Отображает домашнюю страницу"))
+}
+
+func showShippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Отображает определенную заметку"))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Создает новую заметку"))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showShippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Запуск веб-сервера на 4000")
 	err := http.ListenAndServe(":4000", mux)
