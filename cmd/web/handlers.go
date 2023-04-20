5374ea619f4bee2acc164dafb145b8f0e766e14f
xl92@DESKTOP-F8KSG17 <xl92@mail.ru>
2023-04-20 09:47:47 +0300

    Шаблонизатор

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -12,7 +15,22 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Привет из Snippetbox"))
+	files := []string{
+		"./ui/html/home.page.html",
+		"./ui/html/base.layout.html",
+		"./ui/html/footer.partial.html",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
--- test.go
+++ test.go
@@ -1,102 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
-	"strconv"
 )
 
-type Calculator struct {
-	fn, sn int
-}
-
-func (f *Calculator) Add(fn, sn int) int {
-	f.fn = fn
-	f.sn = sn
-	return f.fn + f.sn
-}
-
-func (s *Calculator) Subtract(fn, sn int) int {
-	s.fn = fn
-	s.sn = sn
-	return s.fn - s.sn
-}
-func (m *Calculator) Multiply(fn, sn int) int {
-	m.fn = fn
-	m.sn = sn
-	return m.fn * m.sn
-}
-
-func (d *Calculator) Divide(fn, sn int) int {
-	d.fn = fn
-	d.sn = sn
-	return d.fn / d.sn
-}
-
-type iCalculator interface {
-	Add(fn, sn int) int
-	Subtract(fn, sn int) int
-	Multiply(fn, sn int) int
-	Divide(fn, sn int) int
-}
-
 func main() {
-	var (
-		n1, n2, sign string
-		i            iCalculator
-	)
-
-	f := Calculator{}
-	i = &f
-
-	s := Calculator{}
-	i = &s
-
-	m := Calculator{}
-	i = &m
-
-	d := Calculator{}
-	i = &d
-
-	fmt.Print("Введите первое число: ")
-	fmt.Scanln(&n1)
-	fmt.Print("Введите второе число: ")
-	fmt.Scanln(&n2)
-
-	fn1, err := strconv.Atoi(n1)
+	db, err := sql.Open("mysql", "root:@/mytestbase")
 	if err != nil {
-		log.Println("Только целочисленные значения")
 		log.Fatal(err)
 	}
-	sn2, err := strconv.Atoi(n2)
+	defer db.Close()
+
+	_, err = db.Exec(`
+        CREATE TABLE users (
+            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(30) NOT NULL,
+            email VARCHAR(50) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    `)
 	if err != nil {
-		log.Println("Только целочисленные значения")
 		log.Fatal(err)
-	}
-
-	fmt.Print("Введите оператор (+,-,*,/): ")
-	fmt.Scanln(&sign)
-
-	switch sign {
-	case "+":
-		result := i.Add(fn1, sn2)
-		fmt.Println(result)
-	case "-":
-		result := i.Subtract(fn1, sn2)
-		fmt.Println(result)
-	case "*":
-		result := i.Multiply(fn1, sn2)
-		fmt.Println(result)
-	case "/":
-		if fn1 == 0 || sn2 == 0 {
-			log.Println("Делить на ноль нельзя")
-			return
-		} else {
-			result := i.Divide(fn1, sn2)
-			fmt.Println(result)
-		}
-	default:
-		log.Println("Допустим только один из следующих оператор (+,-,*,/)")
-		return
 	}
 }
